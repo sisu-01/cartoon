5eabdb782d83e4ae8ad5c9f10c9c9275d4e0677b
sisu-01 <tesv010615@naver.com>
2023-11-23 14:53:37 +0900

    내용 간추리기

--- index.js
+++ index.js
@@ -3,74 +3,34 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-const users = [
- { id: 1, name: "유저1" },
- { id: 2, name: "유저2" },
- { id: 3, name: "유저3" }
-];
-
-
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
 app.get("/api/get", (req, res) => {
-    res.json({ok: true, users: users});
+    res.json({ok:true});
 })
 app.get("/api/get/:id", (req, res) => {
     const id = req.params.id
-    const user = users.filter(data => data.id == id);
-    res.json({ok: false, user: user})
+    res.json({ok: false, id: id})
 })
 app.post("/api/post", (req, res) => {
     const { id, name } = req.body;
-    temp = {
-        id: id,
-        name: name,
-    }
-    res.json({ok: true, temp: temp})
+    res.json({ok: true, id: id, name: name})
 })
-app.put("/api/users/update", (req, res) => {
-    
-    // 구조분해를 통해 id 와 name을 추출
-    const { id, name } = req.body
-
-    //map 함수는 자바스크립트에서 배열 함수이다. 요소를 일괄적으로 변경할 때 사용됩니다.
-    const user = users.map(data => {
-
-        if(data.id == id) data.name = name
-
-        return {
-            id: data.id,
-            name: data.name
-        }
-    })
-
-    res.json({ok: true, users: user})
-})
-app.patch("/api/user/update/:id", (req, res) => {
-
+app.put("/api/put/:id", (req, res) => {
     const { id } = req.params
     const { name } = req.body
-
-    //map 함수는 자바스크립트에서 배열 함수이다. 요소를 일괄적으로 변경할 때 사용됩니다.
-    const user = users.map(data => {
-
-        if(data.id == id) data.name = name
-
-        return {
-            id: data.id,
-            name: data.name
-        }
-    })
-
-    res.json({ok: true, users: user})
+    res.json({ok: true, id: id, name: name})
+})
+app.patch("/api/patch/:id", (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+    res.json({ok: true, id: id, name: name})
 })
 app.delete("/api/delete/:id", (req, res) => {
-    const id = req.query.id
-    const user = users.filter(data => data.id != id );
-    res.json({ok: true, users: user})
+    const { id } = req.params;
+    res.json({ok: true, id: id})
 })
 
-// http listen port 생성 서버 실행
 app.listen(3000, () => console.log("run express server"));
