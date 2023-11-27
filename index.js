c45ef57c51c3b693934102db106803b61aeaebd1
sisu-01 <tesv010615@naver.com>
2023-11-27 10:58:46 +0900

    임시 writer api

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+// cartoon list
 app.get('/cartoon/:page', (req, res) => {
     const PAGE = req.params.page || 0;
     const PAGE_START = PAGE * PAGE_LIMIT;
@@ -23,7 +24,7 @@ app.get('/cartoon/:page', (req, res) => {
             res.json({ok:false, err});
             return;
         }
-        conn.query(`select title from cartoon where 1=1 order by id desc limit ${PAGE_START}, ${PAGE_LIMIT};`, (err, result) => {
+        conn.query(`select * from cartoon where 1=1 order by id desc limit ${PAGE_START}, ${PAGE_LIMIT};`, (err, result) => {
             conn.release();
             if(err) {
                 res.json({ok:false, err});
@@ -34,9 +35,40 @@ app.get('/cartoon/:page', (req, res) => {
         })
     });
 })
-app.get('/api/get/:id', (req, res) => {
-    const id = req.params.id
-    res.json({ok: false, id: id})
+// writer list
+app.get('/writer', (req, res) => {
+    const { page, id, nickname } = req.query;
+    const PAGE_LIMIT = 2;
+    const PAGE_START = page * PAGE_LIMIT;
+
+    let sql = '';
+    sql += `select * from writer where 1=1`;
+    if(page === undefined){
+        sql += ` limit 0, ${PAGE_LIMIT}`;
+    }else{
+        sql += ` limit ${PAGE_START}, ${PAGE_LIMIT}`;
+    }
+    sql += ``;
+    sql += ``;
+    console.log(sql);
+    POOL.getConnection((err, conn) => {
+        if (err) {
+            if (conn) {
+                conn.release()
+            }
+            res.json({ok:false, err});
+            return;
+        }
+        conn.query(sql, (err, result) => {
+            conn.release();
+            if(err) {
+                res.json({ok:false, err});
+                return;
+            }else{
+                res.json({ok:true, result});
+            }
+        })
+    });
 })
 app.post('/api/post', (req, res) => {
     const { id, name } = req.body;
