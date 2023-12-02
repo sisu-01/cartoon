44f37a5a81fb5d4f8e50fc3ef2dade028275eaa3
sisu-01 <tesv010615@naver.com>
2023-12-02 15:07:04 +0900

    origin *

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(cors({
-    'origin': [
-        'http://localhost:3000',
-        'http://localhost:3001'
-    ]
+    'origin': '*',
 }));
 
 const PER_PAGE = 5;
