e2c865a992f3d969f70bcd2ab31664eb8458cdd1
sisu-01 <tesv010615@naver.com>
2023-12-04 09:50:19 +0900

    /cartoon /info 개추필터

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ const PER_PAGE = 5;
 app.get('/cartoon', async (req, res) => {
     let temp = Number(req.query.page);
     const page = (temp < 1)? 1 : temp;
+    const order = req.query.order === 'true';
+    const cut = Number(req.query.cut) || false;
     
     let countSql = '';
-    countSql += `SELECT COUNT(*) AS 'count' FROM cartoon`;
-    if (false) {
-        countSql += ` WHERE 1=1`;
-    } else {
-        countSql += ` WHERE recommend > 300`;
+    countSql += `SELECT COUNT(*) AS 'count' FROM cartoon WHERE 1=1`;
+    if (cut) {
+        countSql += ` AND recommend >= ${cut}`;
     }
     const count = await runSql(countSql).then(data => {return data[0]['count']}).catch(() => {return 0});
 
@@ -29,16 +29,14 @@ app.get('/cartoon', async (req, res) => {
 
         const START_PAGE = (page - 1) * PER_PAGE;
         let listSql = '';
-        listSql += `SELECT * FROM cartoon`;
-        if (false) {
-            listSql += ` WHERE 1=1`;
-        } else {
-            listSql += ` WHERE recommend > 300`;
+        listSql += `SELECT * FROM cartoon WHERE 1=1`;
+        if (cut) {
+            countSql += ` AND recommend >= ${cut}`;
         }
-        if (true) {
-            listSql += ` ORDER BY id DESC`;
-        } else {
+        if (order) {
             listSql += ` ORDER BY recommend DESC`;
+        } else {
+            listSql += ` ORDER BY id DESC`;
         }
         listSql += ` limit ${START_PAGE}, ${PER_PAGE}`;
         const list = await runSql(listSql).then(data => {return data}).catch(()=>{return null});
@@ -60,54 +58,19 @@ app.get('/cartoon', async (req, res) => {
     }
 });
 
-app.get('/writer', (req, res) => {
-    const { page, id, nickname } = req.query;
-    const PER_PAGE = 2;
-    const START_PAGE = page * PER_PAGE;
-
-    let sql = '';
-    sql += `select * from writer where 1=1`;
-    if(page === undefined){
-        sql += ` limit 0, ${PER_PAGE}`;
-    }else{
-        sql += ` limit ${START_PAGE}, ${PER_PAGE}`;
-    }
-    sql += ``;
-    sql += ``;
-    console.log(sql);
-    POOL.getConnection((err, conn) => {
-        if (err) {
-            if (conn) {
-                conn.release()
-            }
-            res.json({ok:false, err});
-            return;
-        }
-        conn.query(sql, (err, result) => {
-            conn.release();
-            if(err) {
-                res.json({ok:false, err});
-                return;
-            }else{
-                res.json({ok:true, result});
-            }
-        })
-    });
-})
-
 app.get('/info', async (req, res) => {
     let temp = Number(req.query.page);
     const page = (temp < 1)? 1 : temp;
+    //수정 id랑 nickname sql 인잭션 방지해야돼;
     const id = req.query.id;
     const nickname = req.query.nickname;
     const order = req.query.order === 'true';
     const cut = Number(req.query.cut) || false;
-    console.log(cut);
 
     let countSql = '';
     countSql += `SELECT COUNT(*) AS 'count' FROM cartoon WHERE (writer_id = '${id}' AND writer_nickname = '${nickname}')`;
     if (cut) {
-        countSql += ` AND recommend > ${cut}`;
+        countSql += ` AND recommend >= ${cut}`;
     }
     const count = await runSql(countSql).then(data => {return data[0]['count']}).catch(() => {return 0});
 
@@ -118,7 +81,7 @@ app.get('/info', async (req, res) => {
         listSql += `SELECT * FROM cartoon`;
         listSql += ` WHERE (writer_id = '${id}' AND writer_nickname = '${nickname}')`;
         if (cut) {
-            listSql += ` AND recommend > ${cut}`;
+            listSql += ` AND recommend >= ${cut}`;
         }
         if (order) {
             listSql += ` ORDER BY recommend DESC`;
@@ -126,6 +89,7 @@ app.get('/info', async (req, res) => {
             listSql += ` ORDER BY id DESC`;
         }
         listSql += ` limit ${START_PAGE}, ${PER_PAGE}`;
+        console.log(listSql);
         const list = await runSql(listSql).then(data => {return data}).catch(()=>{return null});
 
         if(list){
