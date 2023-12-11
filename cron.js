713e2a7029eee66041359b62c28b91ab15ede40e
sisu-01 <tesv010615@naver.com>
2023-12-11 17:52:24 +0900

    cron 클린 코드 작성중

--- cron.js
+++ cron.js
@@ -8,33 +8,32 @@ let count = 0;
 //     console.log(count++);
 // });
 
-/**
- * 가장 최신의 만화 id를 가져온다.
- * @param {*} callback
- */
-function getNewestCartoonId(callback) {
-    POOL.getConnection((err, conn) => {
+function runSql(sql, values) {
+    return new Promise((resolve, reject) => {
+       POOL.getConnection((err, conn) => {
         if (err) {
             if (conn) {
-                conn.release();
-                callback(err, false);
+                conn.release()
             }
+            reject(err);
         }
-        conn.query('SELECT id FROM cartoon WHERE 1=1 ORDER BY id DESC LIMIT 1;', (err, result) => {
+        conn.query(sql, values, (err, rows) => {
             conn.release();
-            if(err) {
-                callback(err, false);
-            }else{
-                callback(false, result);
+            if (err) {
+                reject(err);
             }
-        })
+            resolve(rows);
+        });
+      });
     });
-};
+}
+
 /**
  * for문으로 카연갤 페이지 싹 다 크롤링
  * @param {number} NEWEST 가장 최근 만화 id
  */
-const LOOP = 1465;//1463
+const LOOP = 5;//1463
+
 async function crawling(NEWEST) {
     let crawlStop = false;
     for(let i=1; i < LOOP; i++) {
@@ -43,7 +42,6 @@ async function crawling(NEWEST) {
         if(crawlStop){
             break;
         }
-        //await fetch(`https://gall.dcinside.com/board/lists/?id=cartoon&page=${i}&search_pos=&s_type=search_name&s_keyword=.E3.85.87.E3.85.87&exception_mode=recommend`)
         await fetch(`https://gall.dcinside.com/board/lists/?id=cartoon&page=${i}&exception_mode=recommend`)
         .then(response => response.text())
         .then((data) => {
@@ -55,12 +53,6 @@ async function crawling(NEWEST) {
         .catch(error => console.error('Error:', error));
     }
 }
-/**
- * 스크래핑으로 가져온 만화 목록을 db에 하나씩 넣는다.
- * @param {number} NEWEST db에 등록된 가장 최근의 만화 id. 받아온 data의 id가 NEWEST보다 작거나 같으면 db 등록 멈춤.
- * @param {string} HTML 크롤링 해온 page 마크업
- * @param {*} callback 여기로 스크래핑 for문 멈추라고 boolean 보냄
- */
 function setCartoonList(NEWEST, HTML, callback) {
     const $ = cheerio.load(HTML, null, false);
     POOL.getConnection((err, conn) => {
@@ -73,10 +65,10 @@ function setCartoonList(NEWEST, HTML, callback) {
             $('tbody > tr.us-post').map((i, el)=> {
                 const DATE = new Date($(el).find('.gall_date').attr('title'));
                 const isTwoWeek = 14 <= Math.ceil((new Date().setHours(0, 0, 0, 0)-DATE) / (1000 * 3600 * 24));
-                if(!isTwoWeek){
-                    console.log(DATE, '는 너무 일러서 컷');
-                    return;
-                }
+                // if(!isTwoWeek){
+                //     console.log(DATE, '는 너무 일러서 컷');
+                //     return;
+                // }
                 const WRITER_ID = $(el).find('.gall_writer').attr('data-uid') === ''? 'a': $(el).find('.gall_writer').attr('data-uid');
                 const WRITER_VALUES = {
                     id: WRITER_ID,
@@ -109,19 +101,58 @@ function setCartoonList(NEWEST, HTML, callback) {
         return false;
     });
 }
-/**
- * 스케쥴링해서 매일 밤 돌릴 메인 함수
- * @param {boolean} first 기본은 false인데 true를 인자로 넣으면 만화 처음부터 끝까지 다 가져옴.
- */
-function main(first=false) {
-    getNewestCartoonId((err, data) => {
-        if(err){
-            return false;
+
+
+const temp = 2;
+async function scraping(newest) {
+    console.log(`id ${newest}까지 간다`);
+    for (let i=1; i < temp; i++) {
+        console.log(i);
+        const url = `https://gall.dcinside.com/board/lists/?id=cartoon&page=${i}&exception_mode=recommend`;
+        const html = await fetch(url).then(res => res.text());
+
+        const $ = cheerio.load(html, null, false);
+        try {
+            $('tbody > tr.us-post').map(async (i, el)=> {
+                const id = $(el).find('.gall_num').text();
+                if (newest >= id) throw 'break';
+                //수정
+                //This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). The promise rejected with the reason "break".
+                
+                const date = new Date($(el).find('.gall_date').attr('title'));
+                //const isTwoWeek = 14 <= Math.ceil((new Date().setHours(0, 0, 0, 0)-date) / (1000 * 3600 * 24));
+                //if (!isTwoWeek) return;
+                const writer_id = $(el).find('.gall_writer').attr('data-uid') === ''? 'a': $(el).find('.gall_writer').attr('data-uid');
+                const writer_values = {
+                    id: writer_id,
+                    nickname: $(el).find('.gall_writer').attr('data-nick'),
+                }
+                const cartoon_values = {
+                    id: id,
+                    title: $(el).find('.gall_tit > a').first().text().substr(0, 5),
+                    writer_id: writer_id,
+                    writer_nickname: $(el).find('.gall_writer > span > em').text(),
+                    date: date,
+                    recommend: $(el).find('.gall_recommend').text(),
+                }
+                await runSql('INSERT INTO writer SET ?', writer_values).then(data => {return data}).catch(()=>{return []});
+                await runSql('INSERT INTO cartoon SET ?', cartoon_values).then(data => {return data}).catch(()=>{return []});
+            });
+        } catch(e) {
+            if (e === 'break') break;
         }
-        if(data[0] || first) {
-            const NEWEST = first? 1 : data[0]['id'];
-            crawling(NEWEST);
-        }
-    });
+    }
 }
-//main();
+
+async function main(first=false) {
+    const result = await runSql('SELECT id FROM cartoon WHERE 1=1 ORDER BY id DESC LIMIT 1;').then(data => {return data}).catch(()=>{return []});
+
+    if (!first && result[0] === undefined) {
+        return false;
+    } else {
+        const newest = first? 1 : result[0]['id'];
+        scraping(newest);
+    }
+}
+
+main(false);
