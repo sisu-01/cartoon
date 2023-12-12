16a9d3af4fa69ec49bc6e1817d1b9c81d8434577
sisu-01 <tesv010615@naver.com>
2023-12-12 11:11:16 +0900

    cron 리팩토링

--- cron.js
+++ cron.js
@@ -27,11 +27,70 @@ function runSql(sql, values) {
       });
     });
 }
+const loop = 6;
+async function scraping(newest) {
+    console.log(`id ${newest}까지 간다`);
+    for (let i=1; i < loop; i++) {
+        console.log(`${i}페이지`);
+        const url = `https://gall.dcinside.com/board/lists/?id=cartoon&page=${i}&exception_mode=recommend`;
+        const html = await fetch(url).then(res => res.text());
+        const $ = cheerio.load(html, null, false);
 
-/**
- * for문으로 카연갤 페이지 싹 다 크롤링
- * @param {number} NEWEST 가장 최근 만화 id
- */
+        try {
+            $('tbody > tr.us-post').map((i, el)=> {
+                const id = $(el).find('.gall_num').text();
+                if (newest >= id) throw 'break';
+                const values = makeValues($, el);
+                runSql('INSERT IGNORE INTO writer SET ?', values[0])
+                .then(() => {
+                    runSql('INSERT IGNORE INTO cartoon SET ?', values[1])
+                    .then(() => {
+                        console.log(`${i} 사이클`);
+                    }).catch(e => {console.log(e)});
+                }).catch(e => {console.log(e)})
+            });
+        } catch(e) {
+            console.log(e);
+            if (e === 'break') break;
+        }
+    }
+}
+
+function makeValues($, el) {
+    const date = new Date($(el).find('.gall_date').attr('title'));
+    const writer_id = $(el).find('.gall_writer').attr('data-uid') === ''? 'a': $(el).find('.gall_writer').attr('data-uid');
+    const writer_values = {
+        id: writer_id,
+        nickname: $(el).find('.gall_writer').attr('data-nick'),
+    }
+    const cartoon_values = {
+        id: $(el).find('.gall_num').text(),
+        title: $(el).find('.gall_tit > a').first().text().substr(0, 5),
+        writer_id: writer_id,
+        writer_nickname: $(el).find('.gall_writer > span > em').text(),
+        date: date,
+        recommend: $(el).find('.gall_recommend').text(),
+    }
+    return [writer_values, cartoon_values];
+}
+
+async function main(first=false) {
+    const result = await runSql('SELECT id FROM cartoon WHERE 1=1 ORDER BY id DESC LIMIT 1;').then(data => {return data}).catch(()=>{return []});
+
+    if (!first && result[0] === undefined) {
+        return false;
+    } else {
+        const newest = first? 1 : result[0]['id'];
+        scraping(newest);
+    }
+}
+
+main(false);
+//const isTwoWeek = 14 <= Math.ceil((new Date().setHours(0, 0, 0, 0)-date) / (1000 * 3600 * 24));
+//if (!isTwoWeek) return;
+
+
+/*
 const LOOP = 5;//1463
 
 async function crawling(NEWEST) {
@@ -101,58 +160,4 @@ function setCartoonList(NEWEST, HTML, callback) {
         return false;
     });
 }
-
-
-const temp = 2;
-async function scraping(newest) {
-    console.log(`id ${newest}까지 간다`);
-    for (let i=1; i < temp; i++) {
-        console.log(i);
-        const url = `https://gall.dcinside.com/board/lists/?id=cartoon&page=${i}&exception_mode=recommend`;
-        const html = await fetch(url).then(res => res.text());
-
-        const $ = cheerio.load(html, null, false);
-        try {
-            $('tbody > tr.us-post').map(async (i, el)=> {
-                const id = $(el).find('.gall_num').text();
-                if (newest >= id) throw 'break';
-                //수정
-                //This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). The promise rejected with the reason "break".
-                
-                const date = new Date($(el).find('.gall_date').attr('title'));
-                //const isTwoWeek = 14 <= Math.ceil((new Date().setHours(0, 0, 0, 0)-date) / (1000 * 3600 * 24));
-                //if (!isTwoWeek) return;
-                const writer_id = $(el).find('.gall_writer').attr('data-uid') === ''? 'a': $(el).find('.gall_writer').attr('data-uid');
-                const writer_values = {
-                    id: writer_id,
-                    nickname: $(el).find('.gall_writer').attr('data-nick'),
-                }
-                const cartoon_values = {
-                    id: id,
-                    title: $(el).find('.gall_tit > a').first().text().substr(0, 5),
-                    writer_id: writer_id,
-                    writer_nickname: $(el).find('.gall_writer > span > em').text(),
-                    date: date,
-                    recommend: $(el).find('.gall_recommend').text(),
-                }
-                await runSql('INSERT INTO writer SET ?', writer_values).then(data => {return data}).catch(()=>{return []});
-                await runSql('INSERT INTO cartoon SET ?', cartoon_values).then(data => {return data}).catch(()=>{return []});
-            });
-        } catch(e) {
-            if (e === 'break') break;
-        }
-    }
-}
-
-async function main(first=false) {
-    const result = await runSql('SELECT id FROM cartoon WHERE 1=1 ORDER BY id DESC LIMIT 1;').then(data => {return data}).catch(()=>{return []});
-
-    if (!first && result[0] === undefined) {
-        return false;
-    } else {
-        const newest = first? 1 : result[0]['id'];
-        scraping(newest);
-    }
-}
-
-main(false);
+*/
