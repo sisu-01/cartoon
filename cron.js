46c7c7e694bac8388c07e28f4d17e516ede73afd
sisu-01 <tesv010615@naver.com>
2023-12-12 11:29:49 +0900

    정리

--- cron.js
+++ cron.js
@@ -27,7 +27,7 @@ function runSql(sql, values) {
       });
     });
 }
-const loop = 6;
+const loop = 11;
 async function scraping(newest) {
     console.log(`id ${newest}까지 간다`);
     for (let i=1; i < loop; i++) {
@@ -45,7 +45,6 @@ async function scraping(newest) {
                 .then(() => {
                     runSql('INSERT IGNORE INTO cartoon SET ?', values[1])
                     .then(() => {
-                        console.log(`${i} 사이클`);
                     }).catch(e => {console.log(e)});
                 }).catch(e => {console.log(e)})
             });
@@ -65,7 +64,7 @@ function makeValues($, el) {
     }
     const cartoon_values = {
         id: $(el).find('.gall_num').text(),
-        title: $(el).find('.gall_tit > a').first().text().substr(0, 5),
+        title: $(el).find('.gall_tit > a').first().text(),//.substr(0, 5),
         writer_id: writer_id,
         writer_nickname: $(el).find('.gall_writer > span > em').text(),
         date: date,
@@ -88,80 +87,4 @@ function main(first=false) {
         console.log(e);
     });
 }
-main(false);
-
-//const isTwoWeek = 14 <= Math.ceil((new Date().setHours(0, 0, 0, 0)-date) / (1000 * 3600 * 24));
-//if (!isTwoWeek) return;
-
-
-/*
-const LOOP = 5;//1463
-
-async function crawling(NEWEST) {
-    let crawlStop = false;
-    for(let i=1; i < LOOP; i++) {
-        console.log(i);
-        // setCartoonList callback으로 받은 beStop이 true면 크롤링 멈춤.
-        if(crawlStop){
-            break;
-        }
-        await fetch(`https://gall.dcinside.com/board/lists/?id=cartoon&page=${i}&exception_mode=recommend`)
-        .then(response => response.text())
-        .then((data) => {
-            setCartoonList(NEWEST, data, (beStop) => {
-                // callback으로 받은 beStop이 true면 크롤링 멈춤.
-                crawlStop = beStop;
-            });
-        })
-        .catch(error => console.error('Error:', error));
-    }
-}
-function setCartoonList(NEWEST, HTML, callback) {
-    const $ = cheerio.load(HTML, null, false);
-    POOL.getConnection((err, conn) => {
-        if (err) {
-            if (conn) {
-                conn.release()
-            }
-        }
-        try {
-            $('tbody > tr.us-post').map((i, el)=> {
-                const DATE = new Date($(el).find('.gall_date').attr('title'));
-                const isTwoWeek = 14 <= Math.ceil((new Date().setHours(0, 0, 0, 0)-DATE) / (1000 * 3600 * 24));
-                // if(!isTwoWeek){
-                //     console.log(DATE, '는 너무 일러서 컷');
-                //     return;
-                // }
-                const WRITER_ID = $(el).find('.gall_writer').attr('data-uid') === ''? 'a': $(el).find('.gall_writer').attr('data-uid');
-                const WRITER_VALUES = {
-                    id: WRITER_ID,
-                    nickname: $(el).find('.gall_writer').attr('data-nick'),
-                }
-                const CARTOON_VALUES = {
-                    id: $(el).find('.gall_num').text(),
-                    title: $(el).find('.gall_tit > a').first().text().substr(0, 5),
-                    writer_id: WRITER_ID,
-                    writer_nickname: $(el).find('.gall_writer > span > em').text(),
-                    date: DATE,
-                    recommend: $(el).find('.gall_recommend').text(),
-                }
-                // 받아온 리스트를 차례차례 등록하다가,
-                // 기존에 db에 등록돼있던 만화보다 오래되거나 같으면 등록 멈춤
-                if(CARTOON_VALUES['id'] <= NEWEST) {
-                    throw '작거나 같음';
-                }else{
-                    conn.query('INSERT INTO writer SET ?', WRITER_VALUES);
-                    conn.query('INSERT INTO cartoon SET ?', CARTOON_VALUES);
-                }
-            });
-        } catch (e) {
-            conn.release();
-            callback(true);
-            return false;
-        }
-        conn.release();
-        callback(false);
-        return false;
-    });
-}
-*/
+//main(false);
