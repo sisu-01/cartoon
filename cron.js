4f2bec4b032817ea2b1ddcf07654ac1610351cb2
sisu-01 <tesv010615@naver.com>
2023-12-18 16:16:04 +0900

    임시 시리즈 분류 알고리즘

--- cron.js
+++ cron.js
@@ -110,37 +110,126 @@ function main(first=false) {
         console.log(e);
     });
 }
-main(false);
+//main(false);
 
 function test() {
-    runSql(`SELECT title FROM cartoon WHERE ( writer_id = 'toeic945' AND writer_nickname = '급양만와') ORDER BY id ASC;`)
+    //runSql(`SELECT title FROM cartoon WHERE ( writer_id = 'toeic945' AND writer_nickname = '급양만와') ORDER BY id ASC;`)
+    runSql(`SELECT id, title FROM cartoon WHERE writer_nickname = '금요정' ORDER BY id ASC;`)
     .then(list => {
-        console.log(list);
-    })
-    const t1 = '경제툰2) 탐욕의 끝 부동산 대침체4.manhwa';  // 기준
-    const t2 = '경제툰2) 새로운 기술? 인터넷 버블3.manhwa'; // 유사율 52퍼센트
-    const t3 = '경제툰2) 탐욕의 끝 부동산 대침체2.manhwa';  // 유사울 97퍼센트
-    const t4 = '대학원생이 된 천재 돌연변이 고블린 소녀 듀에르코 만화'; //유사율 0퍼센트
-
-    const len = t1.length;
-    const a = 100 / len;
-    let res = levenshtein.get(t1, t2);
-    console.log(len, res, res*a+'%');
-
-    const p1 = '내 약혼녀는zzzz 용 11';
-    const p2 = '내 약혼녀는 용 10';
-    const ll = p2.length; // 더 긴 놈으로?
-    const df = 100 / ll;
-    let z = levenshtein.get(p1, p2);
-    console.log(ll, z, z*df+'%');
+        const series = {};
     
-    /*
-    a = 400개의 게시글
-    a.forEach(i => {
-        a.forEach(j => {
-            const distance = levenshtein.get(i, j);
-        });
-    });
-    */
+        let count = 0;
+        while (true) {
+            if (list.length === 0) {
+                break;
+            }
+
+            const shift = list.shift();
+            const [ base1, base2 ] = splitString(shift['title']);
+
+            let forLoop = list.length;
+            for (let i = 0; i < forLoop;) {
+                const e = list[i];
+                const [ title1, title2 ] = splitString(e['title']);
+                const len1 = base1.length > title1.length? base1.length : title1.length;
+                const len2 = base2.length > title2.length? base2.length : title2.length;
+                const percent1 = 100 / len1;
+                const percent2 = 100 / len2;
+                const distance1 = levenshtein.get(base1, title1);
+                const distance2 = levenshtein.get(base2, title2);
+                const similarity1 = 100 - Math.round(distance1 * percent1);
+                const similarity2 = 100 - Math.round(distance2 * percent2);
+                const sum = similarity1 + similarity2;
+                //수정 글이 길 수록 요구 퍼센트가 낮고 ex)경제툰)쌸라쌸라쌸라쌸라쌸라쌸라쌸라쌸라쌸라쌸라쌸라쌸라쌸라
+                //글이 짧을 수록 요구 서펜트가 높아야 한다ex)살인 -1 살인 -2
+                if (similarity1 >= 50 || similarity2 >= 50) {
+                    //console.log(`##합: ${similarity1+similarity2}% - 유사도: ${similarity1}% |`, title1, `\t\t유사도: ${similarity2}% |`, title2);
+                    if (series[count] === undefined) {
+                        series[count] = [{id: shift['id'], title: shift['title']}];
+                    }
+                    series[count].push({id: e['id'], title: e['title']});
+                    list.splice(i, 1);
+                    forLoop = list.length;
+                } else {
+                    i++;
+                    //console.log(`합: ${similarity1+similarity2}% - 유사도: ${similarity1}% |`, title1, `\t\t유사도: ${similarity2}% |`, title2);
+                }
+            }
+            count++;
+        }
+        console.log(series);
+    })
 }
-//test();
+test();
+//zz();
+
+function zz() {
+    const arr = ['경제1', '경제2', '여우1', '여우2', '수느'];
+
+    while(true) {
+
+        if (arr.length === 0) {
+            break;
+        }
+
+        let bb = arr.length
+        for (let i = 0; i < bb; ) {
+            console.log(i, bb);
+            console.log(arr[i]);
+            //arr.pop();
+            i++;
+            //bb = arr.length;
+        }
+        arr.pop();
+        arr.pop();
+        arr.pop();
+        arr.pop();
+        arr.pop();
+    }
+}
+
+function splitString(str) {
+    let temp = str;
+    temp = temp.replaceAll('MANHWA', '').replaceAll('manhwa', '').replaceAll('MANWHA', '').replaceAll('manwha', '').replaceAll('만화', '').replaceAll('만와', '');
+    temp = temp.replaceAll('프롤로그', '').replaceAll('에필로그', '').replaceAll('마지막화', '');
+    temp = temp.replaceAll('完', '').replaceAll('후기', '').replaceAll('(완)', '');
+    temp = temp.replaceAll('bgm', '').replaceAll('BGM', '');
+    temp = temp.replaceAll('공지', '').replaceAll('휴재', '');
+    temp = temp.replaceAll('ㅇㅎ', '').replaceAll('스압', '');
+    temp = temp.replace(/\d/g, '');//숫자 제거
+    temp = temp.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/ ]/g, '');
+    /*
+    .replaceAll('', '')
+    */
+    temp = temp.trimStart().trimEnd();
+
+    const length = Math.ceil(temp.length / 2);
+    const firstHalf = temp.slice(0, length)
+    const secondHalf = temp.slice(length)
+    return [firstHalf, secondHalf];
+}
+
+function findCommonCharacters(...strings) {
+    // 최소 한 번 등장한 문자를 저장하는 Set
+    const commonCharacters = new Set();
+  
+    if (strings.length < 2) {
+        console.log('최소 두 개 이상의 문자열이 필요합니다.');
+        return '';
+    }
+  
+    // 첫 번째 문자열의 문자를 기준으로 설정
+    const baseString = strings[0];
+  
+    // 각 문자열에 대해 공통 문자 찾기
+    for (let i = 0; i < baseString.length; i++) {
+        const currentChar = baseString[i];
+    
+        if (strings.every(str => str.includes(currentChar))) {
+            commonCharacters.add(currentChar);
+        }
+    }
+  
+    // Set을 배열로 변환하여 출력
+    return Array.from(commonCharacters).join('');
+}
